ef676994f5c048bc08816d4250a7e70783f6e7f7
Bia-source <bia_ferreirad@yahoo.com>
2025-10-16 15:43:06 -0300

    first commit

---
+++ src/main/java/com/delivery/ecommerce/EcommerceDeliveryApplication.java
@@ -0,0 +1,22 @@
+package com.delivery.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class EcommerceDeliveryApplication {
+
+	public static void main(String[] args) {
+		 Dotenv dotenv = Dotenv.load();
+
+        // Define as variáveis no ambiente do Spring
+        System.setProperty("SUPABASE_URL", dotenv.get("SUPABASE_URL"));
+        System.setProperty("SUPABASE_USER", dotenv.get("SUPABASE_USER"));
+        System.setProperty("SUPABASE_PASS", dotenv.get("SUPABASE_PASS"));
+
+		SpringApplication.run(EcommerceDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/ecommerce/EcommerceDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
